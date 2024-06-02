37827bd3f49b97017e48b22c815dda8a27394a90
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-02 20:16:32 +0800

    spring

---
+++ hello-service-8081/src/main/java/com/example/demo/HelloService8081Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloService8081Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloService8081Application.class, args);
+	}
+
+}
---
+++ hello-service-8081/src/test/java/com/example/demo/HelloService8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloService8081ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
