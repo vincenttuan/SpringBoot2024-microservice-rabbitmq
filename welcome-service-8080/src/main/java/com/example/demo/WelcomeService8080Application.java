0d04e2ce975191acc4eab80fc3f749741518cae1
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-02 21:34:17 +0800

    java

--- welcome-service-8080/src/main/java/com/example/demo/WelcomeService8080Application.java
+++ welcome-service-8080/src/main/java/com/example/demo/WelcomeService8080Application.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class WelcomeService8080Application {
@@ -9,5 +10,7 @@ public class WelcomeService8080Application {
 	public static void main(String[] args) {
 		SpringApplication.run(WelcomeService8080Application.class, args);
 	}
-
+	
+	
+	
 }
