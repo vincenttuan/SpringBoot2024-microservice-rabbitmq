4dd390130cc57b50b6f2d9d19bc161177576275a
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-02 22:04:02 +0800

    java

---
+++ welcome-service-8080/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	@GetMapping
+	public String index() {
+		return "Welcome page !";
+	}
+	
+	@GetMapping("/down")
+	public String down() {
+		InstanceInfo instanceInfo = eurekaClient.getApplicationInfoManager().getInfo();
+		instanceInfo.setStatus(InstanceInfo.InstanceStatus.DOWN);
+		return "Welcome service: DOWN";
+	}
+	
+}
