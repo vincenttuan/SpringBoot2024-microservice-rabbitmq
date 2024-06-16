9b24777f6bc0e8820d0115b6605b01def1053701
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-16 19:21:52 +0800

    java

--- feign-customer-service-9092/src/main/java/com/example/demo/dao/CustomerDao.java
+++ feign-customer-service-9092/src/main/java/com/example/demo/dao/CustomerDao.java
@@ -1,5 +1,31 @@
 package com.example.demo.dao;
 
-public class CustomerDao {
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.po.Customer;
+
+@Repository
+public class CustomerDao {
+	
+	private static List<Customer> customers;
+	
+	static {
+		customers = new CopyOnWriteArrayList<>();
+		customers.add(new Customer(1, "Alice", "alice@gmail.com"));
+		customers.add(new Customer(2, "Bob", "bob@gmail.com"));
+		customers.add(new Customer(3, "Cathy", "cathy@gmail.com"));
+	}
+	
+	public List<Customer> findAll() {
+		return customers;
+	}
+	
+	public Optional<Customer> findById(Integer id) {
+		return customers.stream().filter(customer -> customer.getId().equals(id)).findFirst();
+	}
+	
 }
