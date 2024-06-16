c35cec89d1fe8363ce8b95ceb93c1fe3423e86df
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-16 19:15:40 +0800

    java

--- feign-customer-service-9092/src/main/java/com/example/demo/model/response/ApiResponse.java
+++ feign-customer-service-9092/src/main/java/com/example/demo/model/response/ApiResponse.java
@@ -1,5 +1,15 @@
 package com.example.demo.model.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+	private Boolean success;
+	private String message;
+	private T data;
 
 }
