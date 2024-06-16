3bbfd6f28cb1a3736b168fcdf92dcb032b39f8e3
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-16 21:15:58 +0800

    java

--- feign-order-service-9093/src/main/java/com/example/demo/service/OrderService.java
+++ feign-order-service-9093/src/main/java/com/example/demo/service/OrderService.java
@@ -1,5 +1,40 @@
 package com.example.demo.service;
 
-public class OrderService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.OrderDao;
+import com.example.demo.model.dto.OrderDto;
+import com.example.demo.model.po.Customer;
+import com.example.demo.model.po.Order;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderDao orderDao;
+	
+	// Order 單筆訂單 po 轉 dto
+	private OrderDto convertToDto(Order order) {
+		OrderDto orderDto = new OrderDto();
+		orderDto.setId(order.getId());
+		orderDto.setOrderDate(order.getOrderDate());
+		
+		Customer customer = customerClient.getCustomerById(order.getId());
+		orderDto.setCustomer(customer);
+		
+	}
+	
+	// 單筆訂單
+	private OrderDto getOrderById(Integer orderId) {
+		Optional<Order> orderOpt = orderDao.findById(orderId);
+		if(orderOpt.isEmpty()) {
+			return null;
+		}
+		Order order = orderOpt.get();
+		// 將 po 轉 dto
+		return convertToDto(order);
+	}
+	
 }
