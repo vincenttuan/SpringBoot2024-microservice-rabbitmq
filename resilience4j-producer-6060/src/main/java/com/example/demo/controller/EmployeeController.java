e9903e0255cd720163e1e0f50d5a759ce7094d41
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-23 22:03:24 +0800

    re

--- resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 	
+	// 若有錯誤發生會由處理特定的全局異常來處理
 	@GetMapping("/{empId}")
 	public Employee getEmployee(@PathVariable Integer empId) throws InterruptedException {
-		
 		// 若有錯誤發生會由處理特定的全局異常來處理
 		if(empId < 1 ) {
 			throw new RuntimeException("無此員編");
@@ -33,4 +35,36 @@ public class EmployeeController {
 		return emp;
 	}
 	
+	// 若有錯誤發生可以自行處理中斷
+	@CircuitBreaker(name = "employeeCircuitBreaker", fallbackMethod = "getEmployeeFallback")
+	@GetMapping("/breaker/{empId}")
+	public Employee getEmployeeBreaker(@PathVariable Integer empId) throws InterruptedException {
+		// 若有錯誤發生會觸發 CircuitBreaker 而不會由全局異常來處理
+		if(empId < 1 ) {
+			throw new RuntimeException("無此員編");
+		} else if(empId >= 10) {
+			throw new RuntimeException("網路負荷過重連線失敗...");
+		}
+		
+		// 模擬業務處理延遲
+		Thread.sleep(2000);
+		
+		Employee emp = new Employee();
+		emp.setEmpId(empId);
+		emp.setEmpName("John" + empId);
+		emp.setDescription("Manager" + empId);
+		emp.setSalary(30000.0 * empId);
+		
+		return emp;
+	}
+	
+	// 回退方法
+	public Employee getEmployeeFallback(Integer empId, Throwable t) {
+		Employee emp = new Employee();
+		emp.setEmpId(empId);
+		emp.setEmpName("Fallback");
+		emp.setDescription("Fallback Description...");
+		emp.setSalary(null);
+		return emp;
+	}
 }
