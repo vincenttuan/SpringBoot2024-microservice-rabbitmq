a8603170dd990d321371956a3bb8bba52f402510
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-23 20:36:49 +0800

    b

--- loadbalancer-service-b-8081/src/main/java/com/example/demo/controller/ServiceBController.java
+++ loadbalancer-service-b-8081/src/main/java/com/example/demo/controller/ServiceBController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.client.ClientService;
+import com.example.demo.service.RetryService;
 
 @RestController
 public class ServiceBController {
@@ -13,9 +16,17 @@ public class ServiceBController {
 	@Autowired
 	private ClientService clientService;
 	
+	@Autowired
+	private RetryService retryService;
+	
 	@GetMapping("/service-b/{name}")
 	public String getResponse(@PathVariable String name) {
 		return clientService.getResponse(name);
+	}
+	
+	@GetMapping("/retry/service-b/{name}")
+	public String getRetryResponse(@PathVariable String name) throws IOException {
+		return retryService.getRetryResponse(name);
 	}
 	
 	
