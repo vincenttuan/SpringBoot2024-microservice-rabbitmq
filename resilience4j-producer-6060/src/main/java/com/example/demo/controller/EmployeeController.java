05588d5427bbce826677056656a8b5f7de9c0e03
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-23 21:45:13 +0800

    re

--- resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -14,6 +14,12 @@ public class EmployeeController {
 	@GetMapping("/{empId}")
 	public Employee getEmployee(@PathVariable Integer empId) throws InterruptedException {
 		
+		if(empId < 1 ) {
+			throw new RuntimeException("無此員編");
+		} else if(empId >= 10) {
+			throw new RuntimeException("網路負荷過重連線失敗...");
+		}
+		
 		// 模擬業務處理延遲
 		Thread.sleep(2000);
 		
