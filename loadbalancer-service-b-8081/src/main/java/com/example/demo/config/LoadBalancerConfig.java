57d12e00a0980f1cbd107094af4308e906487f4a
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-23 20:11:34 +0800

    b

--- loadbalancer-service-b-8081/src/main/java/com/example/demo/config/LoadBalancerConfig.java
+++ loadbalancer-service-b-8081/src/main/java/com/example/demo/config/LoadBalancerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,8 @@ import feign.Feign;
 // 自行配置附載平衡策略
 @Configuration
 @LoadBalancerClients({
-	@LoadBalancerClient(name = "loadbalancer-service-c-9001", configuration = RandomLoadBalancerConfig.class)
+	//@LoadBalancerClient(name = "loadbalancer-service-c-9001", configuration = RandomLoadBalancerConfig.class),
+	@LoadBalancerClient(name = "loadbalancer-service-c-9001", configuration = RoundRobinLoadBalancerConfig.class)
 })
 public class LoadBalancerConfig {
 	@Bean
@@ -24,10 +26,19 @@ public class LoadBalancerConfig {
 	}
 }
 
-//隨機分配器
+// 隨機分配器
 class RandomLoadBalancerConfig {
 	@Bean
 	public RandomLoadBalancer randomLoadBalancer(LoadBalancerClientFactory clientFactory) {
 		return new RandomLoadBalancer(clientFactory.getLazyProvider("loadbalancer-service-c-9001", ServiceInstanceListSupplier.class), "loadbalancer-service-c-9001");
 	}
 }
+
+// 輪詢分配器
+class RoundRobinLoadBalancerConfig {
+	@Bean
+	public RoundRobinLoadBalancer roundRobinLoadBalancer(LoadBalancerClientFactory clientFactory) {
+		return new RoundRobinLoadBalancer(clientFactory.getLazyProvider("loadbalancer-service-c-9001", ServiceInstanceListSupplier.class), "loadbalancer-service-c-9001");
+	}
+}
+
