066751c8462be765b939d6428a03fad5530bd127
vincenttuan <vincentjava@yahoo.com.tw>
2024-06-30 21:13:58 +0800

    java

--- resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ resilience4j-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -110,6 +111,35 @@ public class EmployeeController {
 		emp.setSalary(30000.0 * empId);
 		
 		return emp;
+	}
+	
+	@Bulkhead(name = "employeeThreadPoolBulkhead", type = Bulkhead.Type.THREADPOOL, 
+			  fallbackMethod = "getCompletableFutureEmployeeFallback")
+	@GetMapping("/threadpool/{empId}")
+	public CompletableFuture<Employee> getEmployeeThreadPool(@PathVariable Integer empId) {
+		return CompletableFuture.supplyAsync(() -> {
+			try {
+				if(empId < 1 ) {
+					throw new RuntimeException("無此員編");
+				} else if(empId >= 10) {
+					throw new RuntimeException("網路負荷過重連線失敗...");
+				}
+				
+				// 模擬業務處理延遲
+				Thread.sleep(2000);
+				
+				Employee emp = new Employee();
+				emp.setEmpId(empId);
+				emp.setEmpName("John" + empId);
+				emp.setDescription("Manager" + empId);
+				emp.setSalary(30000.0 * empId);
+				
+				return emp;
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		
 	}
 	
 	// 回退方法 ------------------------------------------------------------------------
