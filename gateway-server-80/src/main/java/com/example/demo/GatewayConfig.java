bb3843e303cf626b4306751a8738fdc79635fd27
vincenttuan <vincentjava@yahoo.com.tw>
2024-07-14 19:57:35 +0800

    java

--- gateway-server-80/src/main/java/com/example/demo/GatewayConfig.java
+++ gateway-server-80/src/main/java/com/example/demo/GatewayConfig.java
@@ -13,6 +13,7 @@ public class GatewayConfig {
         return builder.routes()
         		
                 .route("feign-customer-service-9092", r -> r.path("/customers/**")
+                		// f: GatewayFilterSpec, c: CircuitBreakerConfig
                 		.filters(f -> f.circuitBreaker(c -> c.setName("customerCircuitBreaker").setFallbackUri("forward:/fallback")))
                         .uri("lb://feign-customer-service-9092"))
                 
