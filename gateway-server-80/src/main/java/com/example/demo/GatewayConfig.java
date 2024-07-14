090d01cd7926bb3f4eed1893fc0ee64baf7accc8
vincenttuan <vincentjava@yahoo.com.tw>
2024-07-14 19:39:15 +0800

    gateway

---
+++ gateway-server-80/src/main/java/com/example/demo/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("customer-service", r -> r.path("/customers/**")
+                        .uri("lb://feign-customer-service-9092"))
+                .route("product-service", r -> r.path("/products/**")
+                        .uri("lb://feign-product-service-9091"))
+                .route("order-service", r -> r.path("/orders/**")
+                        .uri("lb://feign-order-service-9093"))
+                .build();
+    }
+}
