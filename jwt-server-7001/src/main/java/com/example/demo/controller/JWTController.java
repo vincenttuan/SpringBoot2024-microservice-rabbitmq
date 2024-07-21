cd3b315e54ffca48006b88f649ddaffa6127a1fd
vincenttuan <vincentjava@yahoo.com.tw>
2024-07-21 20:35:53 +0800

    jwt

--- jwt-server-7001/src/main/java/com/example/demo/controller/JWTController.java
+++ jwt-server-7001/src/main/java/com/example/demo/controller/JWTController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.JWTServiceNimbus;
+import com.nimbusds.jose.JOSEException;
 
 //Access-Control-Allow-Origin
 @CrossOrigin
 @RestController
 public class JWTController {
 	
+	@Autowired
+	private JWTServiceNimbus jwtServiceNimbus;
 	
+	@GetMapping("/guestJWT")
+	public ResponseEntity<String> getGuestJWT() throws JOSEException {
+		String guestJWT = jwtServiceNimbus.createToken("guest", "user", 600_000);
+		return ResponseEntity.ok(guestJWT);
+	}
 	
 }
