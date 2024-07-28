be18283d99d09947720fdc09bc372a04967a50d0
vincenttuan <vincentjava@yahoo.com.tw>
2024-07-28 21:34:15 +0800

    jwt

---
+++ jwt-order-7002/src/main/java/com/example/demo/JwtOrder7002Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOrder7002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOrder7002Application.class, args);
+	}
+
+}
