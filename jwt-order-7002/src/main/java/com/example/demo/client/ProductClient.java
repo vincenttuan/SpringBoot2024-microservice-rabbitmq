27c256235ee93226bf7067fe5788ec382e79d3c8
vincenttuan <vincentjava@yahoo.com.tw>
2024-07-28 22:06:59 +0800

    jwt

--- jwt-order-7002/src/main/java/com/example/demo/client/ProductClient.java
+++ jwt-order-7002/src/main/java/com/example/demo/client/ProductClient.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "JWT-PRODUCT-7003")
+import com.example.demo.config.FeignClientConfig;
+
+@FeignClient(name = "JWT-PRODUCT-7003", configuration = FeignClientConfig.class)
 public interface ProductClient {
 	@GetMapping("/products")
 	List<String> findAllProducts();
