f4b9e1fd8914988eadf1ae9b82452fd4b8b21943
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 21:43:30 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/publish_subscribe/RabbitMQConfig.java
+++ rabbit-mq/src/main/java/com/example/demo/publish_subscribe/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package com.example.demo.publish_subscribe;
 
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+
+
 /******************************************************
 							 news.website				
 	                          +-------+
@@ -16,6 +21,27 @@ P發新聞 -> |  Exchange   |---+
  * Queue: 用於緩存消息，等待消費者消費。
  * Binding: 將隊列綁定到交換機的配置。
 *******************************************************/
+// RabbitMQ 設定類，用於配置交換機和隊列。
 public class RabbitMQConfig {
+	
+	// FanoutExchange: 用於將消息廣播到所有綁定的隊列。
+	@Bean
+	public FanoutExchange fanoutExchange() {
+		return new FanoutExchange("news.fanout");
+	}
+	
+	// Queue: 用於緩存消息，等待消費者消費。
+	@Bean
+	public Queue websiteQueue() {
+		return new Queue("news.website");
+	}
+	
+	@Bean
+	public Queue appQueue() {
+		return new Queue("news.app");
+	}
+	
+	
+	
 	
 }
