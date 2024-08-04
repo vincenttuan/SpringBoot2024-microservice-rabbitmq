549b9ecc152d648ce5174ab0ef792e9fa5a48e70
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 21:57:17 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsSubscriber.java
+++ rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsSubscriber.java
@@ -1,5 +1,28 @@
 package com.example.demo.publish_subscribe;
 
-public class NewsSubscriber {
+import java.util.Date;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用於接收新聞消息的訂閱者。
+ * 使用 @RabbitListener 註解來實現消息接收：
+ * @RabbitListener: 註冊消息監聽器並指定需要監聽的隊列。
+ */
+@Service
+public class NewsSubscriber {
+	
+	// 從 news.website 隊列中接收到新聞消息(監聽 news.website 隊列)
+	@RabbitListener(queues = "news.website")
+	public void reveiveFromWebsite(String news) {
+		System.out.println("Website 收到: " + news + " " + new Date());
+	}
+	
+	// 從 news.app 隊列中接收到新聞消息(監聽 news.app 隊列)
+	@RabbitListener(queues = "news.website")
+	public void reveiveFromApp(String news) {
+		System.out.println("App 收到: " + news + " " + new Date());
+	}
+	
 }
