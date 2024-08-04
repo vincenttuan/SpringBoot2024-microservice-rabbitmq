2fd76526d69caf813d1dbff65cc0a9ef69ad407c
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 21:49:08 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsPublisher.java
+++ rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsPublisher.java
@@ -1,5 +1,13 @@
 package com.example.demo.publish_subscribe;
 
+import org.springframework.stereotype.Service;
+
+/**
+ * 用於發送新聞消息的服務。
+ * RabbitTemplate: 提供了許多便捷的方法來發送和接收消息。
+ * convertAndSend: 發送消息到指定的交換機。
+ */
+@Service
 public class NewsPublisher {
 
 }
