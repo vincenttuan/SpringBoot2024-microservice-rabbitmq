49336a36d9c6ab323c1db46c097f2360f6c3e4ed
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 19:20:43 +0800

    jwt

---
+++ jwt-client-7000/src/main/java/com/example/demo/JwtClient7000Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtClient7000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtClient7000Application.class, args);
+	}
+
+}
