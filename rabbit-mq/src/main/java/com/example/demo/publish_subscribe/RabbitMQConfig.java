8b157ac59892531cfb9af95bca5316f8647faf7c
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 21:38:40 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/publish_subscribe/RabbitMQConfig.java
+++ rabbit-mq/src/main/java/com/example/demo/publish_subscribe/RabbitMQConfig.java
@@ -1,5 +1,21 @@
 package com.example.demo.publish_subscribe;
 
+/**
+							 news.website				
+	                          +-------+
+	       news.fanout     +--| Queue | -> C1 網站接收新聞
+	     +-------------+   |  +-------+
+P發新聞 -> |  Exchange   |---+
+	     +-------------+   |  +-------+
+	                       +--| Queue | -> C2 App接收新聞
+	                          +-------+
+	                          news.app
+ 
+ * RabbitMQ 設定類，用於配置交換機和隊列。
+ * FanoutExchange: 用於將消息廣播到所有綁定的隊列。
+ * Queue: 用於緩存消息，等待消費者消費。
+ * Binding: 將隊列綁定到交換機的配置。
+*/
 public class RabbitMQConfig {
 
 }
