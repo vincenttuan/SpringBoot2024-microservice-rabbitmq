283a6b1af22cd479b35e163fccaccfb8b500decf
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-04 22:02:10 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsApplication.java
+++ rabbit-mq/src/main/java/com/example/demo/publish_subscribe/NewsApplication.java
@@ -1,9 +1,28 @@
 package com.example.demo.publish_subscribe;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 啟動
+// 啟動新聞發布系統
 @SpringBootApplication
-public class NewsApplication {
+public class NewsApplication implements CommandLineRunner {
+	
+	private NewsPublisher newsPublisher;
+	
+	public NewsApplication(NewsPublisher newsPublisher) {
+		this.newsPublisher = newsPublisher;
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		newsPublisher.publishNews("倫敦奧運-羽球金牌戰 10:00 線上直播");
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApplication.class, args);
+	}
+	
+	
+	
 }
