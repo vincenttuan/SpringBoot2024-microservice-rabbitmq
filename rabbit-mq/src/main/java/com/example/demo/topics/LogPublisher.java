6d1581b725849966f6fa4cf853deb72ab43f1707
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 20:13:45 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/topics/LogPublisher.java
+++ rabbit-mq/src/main/java/com/example/demo/topics/LogPublisher.java
@@ -1,5 +1,6 @@
 package com.example.demo.topics;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +9,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LogPublisher {
-
+	
+	private RabbitTemplate rabbitTemplate;
+	
+	public LogPublisher(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	/**
+     * 發送日誌消息到 TopicExchange。
+     *
+     * @param routingKey 路由鍵，決定消息的路由方式(就是要傳到哪一個 Queue)
+     * @param message    要發送的日誌消息
+     */
+	public void publishLog(String routingKey, String message) {
+		// 發送消息到 topic exchange 根據主題, 路由鍵, 消息
+		rabbitTemplate.convertAndSend("logs.topic", routingKey, message);
+	}
 }
