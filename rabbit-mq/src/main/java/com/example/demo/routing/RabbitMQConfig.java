33dafa84d22b0f29f153e6883a901de51595f6bd
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 19:15:41 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/routing/RabbitMQConfig.java
+++ rabbit-mq/src/main/java/com/example/demo/routing/RabbitMQConfig.java
@@ -1,6 +1,24 @@
 package com.example.demo.routing;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+/******************************************************
+
+							 				
+	                         digital  +-------+
+	      orders.direct    +--------->| Queue | -> C1 (orders.digital) 
+	     +-------------+   | clothing +-------+
+    P -> |  Exchange   |---+--------->| Queue | -> C2 (orders.clothing)
+	     +-------------+   | food     +-------+
+	                       +--------->| Queue | -> C3 (orders.food)
+	                                  +-------+
+	                          
+
+
+*******************************************************/
 
 /**
  * RabbitMQ 設定類，用於配置交換機和隊列。
@@ -9,5 +27,12 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitMQConfig {
-
+	
+	// 配置一個 DirectExchange
+	// DirectExchange 會根據路由鍵將消息發送到特定隊列中
+	@Bean
+	public DirectExchange directExchange() {
+		return new DirectExchange("orders.direct");
+	}
+	
 }
