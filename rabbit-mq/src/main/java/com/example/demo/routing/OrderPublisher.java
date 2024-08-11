c1c42884f1699afb494781c74f0dd001e38b6eaf
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 19:36:27 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/routing/OrderPublisher.java
+++ rabbit-mq/src/main/java/com/example/demo/routing/OrderPublisher.java
@@ -1,5 +1,6 @@
 package com.example.demo.routing;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +9,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderPublisher {
-
+	
+	private RabbitTemplate rabbitTemplate;
+	
+	public OrderPublisher(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	/**
+	 * 發送訂單消息
+	 * @param type  訂單類型(digital(數位), clothing(服裝), food(食品)) 就是 routingKey(路由鍵)
+	 * @param order 訂單詳情
+	*/
+	public void publishOrder(String type, String order) {
+		// 發送消息到 direct exchange
+		rabbitTemplate.convertAndSend("orders.direct", type, order);
+	}
 }
