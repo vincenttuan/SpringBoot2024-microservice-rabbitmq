fbfc0d347fa0afb52b6fad9d7fe0ab4e502ce475
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 20:10:04 +0800

    mq

---
+++ rabbit-mq/src/main/java/com/example/demo/topics/LogPublisher.java
@@ -0,0 +1,12 @@
+package com.example.demo.topics;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 用於發送日誌消息的服務。
+ * publishLog: 根據指定的路由鍵發送日誌消息。
+ */
+@Service
+public class LogPublisher {
+
+}
