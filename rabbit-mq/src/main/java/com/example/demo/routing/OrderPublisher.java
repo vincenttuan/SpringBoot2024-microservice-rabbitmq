c7f7493e47e3516d69659c4048de1d0a246ab39d
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 19:31:28 +0800

    mq

---
+++ rabbit-mq/src/main/java/com/example/demo/routing/OrderPublisher.java
@@ -0,0 +1,12 @@
+package com.example.demo.routing;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 用於發送訂單消息的服務。
+ * 發送消息時指定路由鍵，以確保消息正確路由到目標隊列。
+ */
+@Service
+public class OrderPublisher {
+
+}
