d88e6d576ca660f08d5f991517ee94737c3019e9
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 20:03:31 +0800

    mq

---
+++ rabbit-mq/src/main/java/com/example/demo/topics/RabbitMQConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.topics;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ 設定類，用於配置交換機和隊列。
+ * TopicExchange: 用於將消息根據主題路由鍵發送到匹配的隊列。
+ * 使用 # 表示匹配多個單詞，* 表示匹配一個單詞。
+ */
+@Configuration
+public class RabbitMQConfig {
+	
+}
