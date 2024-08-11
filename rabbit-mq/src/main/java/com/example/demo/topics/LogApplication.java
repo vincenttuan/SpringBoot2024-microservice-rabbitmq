437cc2ad336a9a56f44304991d4b2ae820704903
vincenttuan <vincentjava@yahoo.com.tw>
2024-08-11 20:22:15 +0800

    mq

--- rabbit-mq/src/main/java/com/example/demo/topics/LogApplication.java
+++ rabbit-mq/src/main/java/com/example/demo/topics/LogApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo.topics;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -14,9 +15,14 @@ public class LogApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		logPublisher.publishLog("app.info", "Application started successfully.");
+		logPublisher.publishLog("app.error", "Null pointer exception occurred.");
+		logPublisher.publishLog("database.update", "Updated user details.");
+		logPublisher.publishLog("database.query.all", "Executed SELECT query.");
 	}
 	
-	
+	public static void main(String[] args) {
+		SpringApplication.run(LogApplication.class, args);
+	}
 
 }
